66a8fa809857d2da736e34fdc7a94a35b90ec46c
F190935 <f190935@nu.edu.pk>
2023-08-26 20:37:34 +0500

    Define User Scheme with backend validation

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,19 @@ const app = express()
 
 const PORT = process.env.PORT || 5000;
 
+//Middlewares
+app.use(express.json())
+
+app.use(express.urlencoded({extended: false}))
+
+app.use(bodyParser.json())
+
+//Routes
+app.get("/", (req,res)=>{
+    res.send("HOME PAGE");
+});
+
+
 //Connect to DB to start server
 mongoose
     .connect(process.env.MONGO_URI)
